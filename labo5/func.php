c37ca20bf3640873e66bab9f67baf85aaa7b4512
Francis Begyn <francis.begyn@gmail.com>
2018-11-11 00:33:40 +0100

    Working dijkstra (at least I think so)

--- labo5/func.php
+++ labo5/func.php
@@ -1,31 +1,31 @@
 <?php
 
 /*
-	author Maarten Slembrouck <maarten.slembrouck@gmail.com>
-*/
+  author Maarten Slembrouck <maarten.slembrouck@gmail.com>
+ */
 
 function initialize_mysql_connection(){
-	global $servername;
-	global $username;
-	global $password;
-	global $dbname;
+  global $servername;
+  global $username;
+  global $password;
+  global $dbname;
 
-	// Create connection
-	$mysqli = new mysqli($servername, $username, $password, $dbname);
+  // Create connection
+  $mysqli = new mysqli($servername, $username, $password, $dbname);
 
-	if ($mysqli->connect_errno) {
-		echo "Sorry, this website is experiencing problems.";
-		echo "Error: Failed to make a MySQL connection, here is why: \n";
-		echo "Errno: " . $mysqli->connect_errno . "\n";
-		echo "Error: " . $mysqli->connect_error . "\n";
-		exit;
-	}
-        return $mysqli;
+  if ($mysqli->connect_errno) {
+    echo "Sorry, this website is experiencing problems.";
+    echo "Error: Failed to make a MySQL connection, here is why: \n";
+    echo "Errno: " . $mysqli->connect_errno . "\n";
+    echo "Error: " . $mysqli->connect_error . "\n";
+    exit;
+  }
+  return $mysqli;
 }
 
 function close_mysql_connection(){
-	global $mysqli;
-	$mysqli->close();
+  global $mysqli;
+  $mysqli->close();
 }
 
 function getMinMaxLatLon(){
@@ -65,25 +65,25 @@ function distance($lat1,$lon1,$lat2,$lon2){
 function getNodeId($from_lat, $from_lon, $transport){
   // find the closest node_id to ($from_lat, $from_lon) on a way
   global $mysqli;
-  $offset = 0.5;
+  $offset = 0.00069;
   switch ($transport){
   case "foot":
-    $sql = "SELECT id, (lat-{$from_lat})*(lat-{$from_lat}) + (lon - {$from_lon})*(lon - {$from_lon}) AS x FROM osm_nodes
-           	WHERE lat < $from_lat+$offset AND lat > $from_lat-$offset
-            AND lon < $from_lon+$offset AND lon > $from_lon-$offset
-						ORDER BY x ASC LIMIT 1";
+    $sql = "SELECT id, (lat-{$from_lat})*(lat-{$from_lat}) + (lon - {$from_lon})*(lon - {$from_lon}) AS x FROM osm_road_nodes
+      WHERE lat < $from_lat+$offset AND lat > $from_lat-$offset
+      AND lon < $from_lon+$offset AND lon > $from_lon-$offset
+      ORDER BY x ASC LIMIT 1";
   case "bicycle":
-    $sql = "SELECT id, (lat-{$from_lat})*(lat-{$from_lat}) + (lon - {$from_lon})*(lon - {$from_lon}) AS x FROM osm_nodes
-            WHERE lat < $from_lat+$offset AND lat > $from_lat-$offset
-            AND lon < $from_lon+$offset AND lon > $from_lon-$offset
-						ORDER BY x ASC LIMIT 1";
+    $sql = "SELECT id, (lat-{$from_lat})*(lat-{$from_lat}) + (lon - {$from_lon})*(lon - {$from_lon}) AS x FROM osm_road_nodes
+      WHERE lat < $from_lat+$offset AND lat > $from_lat-$offset
+      AND lon < $from_lon+$offset AND lon > $from_lon-$offset
+      ORDER BY x ASC LIMIT 1";
   case "car":
-    $sql = "SELECT id, (lat-{$from_lat})*(lat-{$from_lat}) + (lon - {$from_lon})*(lon - {$from_lon}) AS x FROM osm_nodes
-            WHERE lat < $from_lat+$offset AND lat > $from_lat-$offset
-            AND lon < $from_lon+$offset AND lon > $from_lon-$offset
-						ORDER BY x ASC LIMIT 1";
+    $sql = "SELECT id, (lat-{$from_lat})*(lat-{$from_lat}) + (lon - {$from_lon})*(lon - {$from_lon}) AS x FROM osm_road_nodes
+      WHERE lat < $from_lat+$offset AND lat > $from_lat-$offset
+      AND lon < $from_lon+$offset AND lon > $from_lon-$offset
+      ORDER BY x ASC LIMIT 1";
   }
-	$arr = array();
+  $arr = array();
   $result =  $mysqli->query($sql);
   while($result && $row = $result->fetch_assoc()){
     $arr[] = $row;
@@ -94,13 +94,13 @@ function getNodeId($from_lat, $from_lon, $transport){
 function costMatrix($transport){
   $mysqli = initialize_mysql_connection();
   $costM = array();
-	switch ($transport){
-	case "foot":
+  switch ($transport){
+  case "foot":
     $distance_info = $mysqli->query("SELECT DISTINCT a.node_id, a.neighbour_id, a.distance from osm_node_neighbours_walk AS a");
-	case "bicycle":
-    $distance_info = $mysqli->query("SELECT DISTINCT a.node_id, a.neighbour_id, a.distance from osm_node_neighbours_cycle AS a");
+  case "bicycle":
+    $distance_info = $mysqli->query("SELECT DISTINCT a.node_id, a.neighbour_id, a.distance from osm_node_neighbours_bike AS a");
   case "car":
-    $distance_info = $mysqli->query("SELECT DISTINCT a.node_id, a.neighbour_id, a.distance from osm_node_neighbours_drive AS a");
+    $distance_info = $mysqli->query("SELECT DISTINCT a.node_id, a.neighbour_id, a.distance from osm_node_neighbours_walk AS a");
   }
   while($row = $distance_info->fetch_assoc()){
     $costM[$row['node_id']][$row['neighbour_id']] = $row['distance'];
@@ -109,41 +109,50 @@ function costMatrix($transport){
 }
 
 function getShortestPathDijkstra($a, $b, $transport){
-	// Create costMatrix
+  // Create costMatrix
   $_distArr = costMatrix($transport);
-	//initialize the array for storing
-	$S = array();//the nearest path with its parent and weight
-	$Q = array();//the left nodes without the nearest path
-	foreach(array_keys($_distArr) as $val) $Q[$val] = INF;
-	$Q[$a] = 0;
+  //initialize the array for storing
+  $S = array();//the nearest path with its parent and weight
+  $Q = array();//the left nodes without the nearest path
+  foreach(array_keys($_distArr) as $val) $Q[$val] = INF;
+  $Q[$a] = 0;
 
-	if (!array_key_exists($b, $S)) {
-    echo "Found no way.";
+  // No path exists between nodes, stop
+  if (!array_key_exists($b, $Q)) {
     return;
-	}
+  }
 
-	//start calculating
-	while(!empty($Q)){
-			$min = array_search(min($Q), $Q);//the most min weight
-			if($min == $b) break;
-			foreach($_distArr[$min] as $key=>$val) if(!empty($Q[$key]) && $Q[$min] + $val < $Q[$key]) {
-					$Q[$key] = $Q[$min] + $val;
-					$S[$key] = array($min, $Q[$key]);
-			}
-			unset($Q[$min]);
-	}
+  //start calculating
+  while(!empty($Q)){
+    $min = array_search(min($Q), $Q);//the most min weight
+    if($min == $b) break;
+    foreach($_distArr[$min] as $key=>$val) if(!empty($Q[$key]) && $Q[$min] + $val < $Q[$key]) {
+      $Q[$key] = $Q[$min] + $val;
+      $S[$key] = array($min, $Q[$key]);
+    }
+    unset($Q[$min]);
+    // We found the target, time to stop
+    if (array_key_exists($b,$S)){
+      break;
+    }
+  }
 
-	//list the path
-	$path = array();
-	$pos = $b;
-	while($pos != $a){
-			$path[] = $pos;
-			$pos = $S[$pos][0];
-	}
-	$path[] = $a;
-	$path = array_reverse($path);
-	
-	return array($S[$to_node][1],$path);
+  // No path exists between nodes, stop
+  if (!array_key_exists($b, $S)) {
+    return;
+  }
+
+  //list the path
+  $path = array();
+  $pos = $b;
+  while($pos != $a){
+    $path[] = $pos;
+    $pos = $S[$pos][0];
+  }
+  $path[] = $a;
+  $path = array_reverse($path);
+
+  return array($S[$b][1],$path);
 }
 
 function compareNodes($a, $b){
@@ -163,10 +172,10 @@ function json_dijkstra($from_lat, $from_lon, $to_lat, $to_lon, $transport){
   // throw new Exception("Error: ...");
 
   $output = array(
-      "from_node" => $from_node,
-      "to_node" => $to_node,
-      "path" => $path,
-      "distance" => $distance
+    "from_node" => $from_node,
+    "to_node" => $to_node,
+    "path" => $path,
+    "distance" => $distance
   );
 
   return json_encode($output);
